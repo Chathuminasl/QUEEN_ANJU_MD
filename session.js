16c7f0474acc260ae86f7aa6455e90bebc8399df
Chathuminasl <149718906+Chathuminasl@users.noreply.github.com>
2024-12-07 21:30:18 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=6N8mmDoK#lPMWm7Sq6GYHuwI3HFoid12ss6rnjerY8hh-3SjWjkM",
 };
